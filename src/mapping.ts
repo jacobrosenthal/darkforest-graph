77a65c6692ba89311aa93334768af831bdcd0f08
Scott Moses Sunarto <github@smsunarto.com>
2020-10-13 15:04:14 +0700

    Draft

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,99 +1,165 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address } from "@graphprotocol/graph-ts";
 import {
-  Contract,
-  ArrivalQueued,
-  BoughtHat,
-  PlanetDelegated,
-  PlanetUndelegated,
-  PlanetUpgraded,
-  PlayerInitialized
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+    Contract,
+    PlanetUpgraded,
+    PlayerInitialized,
+    RefreshPlanetCall,
+} from "../generated/Contract/Contract";
+import { Player, Planet } from "../generated/schema";
 
-export function handleArrivalQueued(event: ArrivalQueued): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.arrivalId = event.params.arrivalId
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DISABLE_ZK_CHECK(...)
-  // - contract.PERLIN_THRESHOLD_1(...)
-  // - contract.PERLIN_THRESHOLD_2(...)
-  // - contract.PLANET_RARITY(...)
-  // - contract.SILVER_RARITY_1(...)
-  // - contract.SILVER_RARITY_2(...)
-  // - contract.SILVER_RARITY_3(...)
-  // - contract.TIME_FACTOR_HUNDREDTHS(...)
-  // - contract._locationIdValid(...)
-  // - contract.adminAddress(...)
-  // - contract.bulkGetPlanetIds(...)
-  // - contract.bulkGetPlanets(...)
-  // - contract.bulkGetPlanetsExtendedInfo(...)
-  // - contract.bulkGetPlayers(...)
-  // - contract.cumulativeRarities(...)
-  // - contract.getDefaultStats(...)
-  // - contract.getNPlanets(...)
-  // - contract.getNPlayers(...)
-  // - contract.getPlanetArrivals(...)
-  // - contract.getPlanetCounts(...)
-  // - contract.getPlanetCumulativeRarities(...)
-  // - contract.getPlanetLevelThresholds(...)
-  // - contract.initializedPlanetCountByLevel(...)
-  // - contract.isDelegated(...)
-  // - contract.isPlayerInitialized(...)
-  // - contract.paused(...)
-  // - contract.planetArrivals(...)
-  // - contract.planetDefaultStats(...)
-  // - contract.planetEvents(...)
-  // - contract.planetEventsCount(...)
-  // - contract.planetIds(...)
-  // - contract.planetLevelThresholds(...)
-  // - contract.planets(...)
-  // - contract.planetsExtendedInfo(...)
-  // - contract.playerIds(...)
-  // - contract.upgradePlanet(...)
-  // - contract.upgrades(...)
-  // - contract.worldRadius(...)
+function toSpaceType(spaceType: string): string {
+    if (spaceType == "0") {
+        return "NEBULA";
+    } else if (spaceType == "1") {
+        return "SPACE";
+    } else {
+        return "DEEP_SPACE";
+    }
 }
 
-export function handleBoughtHat(event: BoughtHat): void {}
+function toPlanetResource(planetResource: string): string {
+    if (planetResource == "0") {
+        return "NONE";
+    } else {
+        return "SILVER";
+    }
+}
 
-export function handlePlanetDelegated(event: PlanetDelegated): void {}
+export function handlePlayerInitialized(event: PlayerInitialized): void {
+    let playerEntity = Player.load(event.params.player.toHex());
+    let planetEntity = Planet.load(event.params.loc.toString());
+    let contract = Contract.bind(event.address);
 
-export function handlePlanetUndelegated(event: PlanetUndelegated): void {}
+    if (playerEntity == null) {
+        playerEntity = new Player(event.params.player.toHex());
 
-export function handlePlanetUpgraded(event: PlanetUpgraded): void {}
+        playerEntity.initTimestamp = event.block.timestamp;
+        playerEntity.homeWorld = event.params.loc;
+    }
 
-export function handlePlayerInitialized(event: PlayerInitialized): void {}
+    if (planetEntity == null) {
+        planetEntity = new Planet(event.params.loc.toString());
+
+        let planet = contract.planets(event.params.loc);
+        let planetExtendedInfo = contract.planetsExtendedInfo(event.params.loc);
+
+        planetEntity.isInitialized = planetExtendedInfo.value0;
+        planetEntity.createdAt = planetExtendedInfo.value1;
+        planetEntity.lastUpdated = planetExtendedInfo.value2;
+        planetEntity.owner = planet.value0.toHexString();
+        planetEntity.perlin = planetExtendedInfo.value3;
+        planetEntity.range = planet.value1;
+        planetEntity.speed = planet.value2;
+        planetEntity.defense = planet.value3;
+        planetEntity.population = planet.value4;
+        planetEntity.populationCap = planet.value5;
+        planetEntity.populationGrowth = planet.value6;
+        planetEntity.silverCap = planet.value8;
+        planetEntity.silverGrowth = planet.value9;
+        planetEntity.silver = planet.value10;
+        planetEntity.planetLevel = planet.value11;
+        planetEntity.upgradeState0 = planetExtendedInfo.value5;
+        planetEntity.upgradeState1 = planetExtendedInfo.value6;
+        planetEntity.upgradeState2 = planetExtendedInfo.value7;
+        planetEntity.hatLevel = planetExtendedInfo.value8;
+        planetEntity.planetResource = toPlanetResource(
+            planet.value7.toString()
+        );
+        planetEntity.spaceType = toSpaceType(
+            planetExtendedInfo.value4.toString()
+        );
+    }
+
+    playerEntity.save();
+    planetEntity.save();
+}
+
+// export function handleArrivalQueued(event: ArrivalQueued): void {}
+
+// export function handleBoughtHat(event: BoughtHat): void {}
+
+// export function handlePlanetDelegated(event: PlanetDelegated): void {}
+
+// export function handlePlanetUndelegated(event: PlanetUndelegated): void {}
+
+// export function handlePlanetUpgraded(event: PlanetUpgraded): void {
+//     let entity = Planet.load(event.params.loc.toString());
+//     let contract = Contract.bind(event.address);
+
+//     if (entity == null) {
+//         entity = new Planet(event.params.loc.toString());
+
+//         let planet = contract.planets(event.params.loc);
+//         let planetExtendedInfo = contract.planetsExtendedInfo(event.params.loc);
+
+//         entity.isInitialized = planetExtendedInfo.value0;
+//         entity.createdAt = planetExtendedInfo.value1;
+//         entity.lastUpdated = planetExtendedInfo.value2;
+//         entity.owner = planet.value0.toHexString();
+//         entity.perlin = planetExtendedInfo.value3;
+//         entity.range = planet.value1;
+//         entity.speed = planet.value2;
+//         entity.defense = planet.value3;
+//         entity.population = planet.value4;
+//         entity.populationCap = planet.value5;
+//         entity.populationGrowth = planet.value6;
+//         entity.silverCap = planet.value8;
+//         entity.silverGrowth = planet.value9;
+//         entity.silver = planet.value10;
+//         entity.planetLevel = planet.value11;
+//         entity.upgradeState0 = planetExtendedInfo.value5;
+//         entity.upgradeState1 = planetExtendedInfo.value6;
+//         entity.upgradeState2 = planetExtendedInfo.value7;
+//         entity.hatLevel = planetExtendedInfo.value8;
+//         entity.planetResource = toPlanetResource(planet.value7.toString());
+//         entity.spaceType = toSpaceType(planetExtendedInfo.value4.toString());
+//     } else {
+//         let planetExtendedInfo = contract.planetsExtendedInfo(event.params.loc);
+
+//         entity.upgradeState0 = planetExtendedInfo.value5;
+//         entity.upgradeState1 = planetExtendedInfo.value6;
+//         entity.upgradeState2 = planetExtendedInfo.value7;
+//     }
+
+//     entity.save();
+// }
+
+export function handleRefreshPlanet(call: RefreshPlanetCall): void {
+    let entity = Planet.load(call.inputs._location.toString());
+    let contract = Contract.bind(
+        Address.fromString(call.transaction.to.toString())
+    );
+
+    if (entity == null) {
+        entity = new Planet(call.inputs._location.toString());
+    }
+
+    let planet = contract.planets(call.inputs._location);
+    let planetExtendedInfo = contract.planetsExtendedInfo(
+        call.inputs._location
+    );
+
+    entity.isInitialized = planetExtendedInfo.value0;
+    entity.createdAt = planetExtendedInfo.value1;
+    entity.lastUpdated = planetExtendedInfo.value2;
+    entity.owner = planet.value0.toHexString();
+    entity.perlin = planetExtendedInfo.value3;
+    entity.range = planet.value1;
+    entity.speed = planet.value2;
+    entity.defense = planet.value3;
+    entity.population = planet.value4;
+    entity.populationCap = planet.value5;
+    entity.populationGrowth = planet.value6;
+    entity.silverCap = planet.value8;
+    entity.silverGrowth = planet.value9;
+    entity.silver = planet.value10;
+    entity.planetLevel = planet.value11;
+    entity.upgradeState0 = planetExtendedInfo.value5;
+    entity.upgradeState1 = planetExtendedInfo.value6;
+    entity.upgradeState2 = planetExtendedInfo.value7;
+    entity.hatLevel = planetExtendedInfo.value8;
+    entity.planetResource = toPlanetResource(planet.value7.toString());
+    entity.spaceType = toSpaceType(planetExtendedInfo.value4.toString());
+
+    entity.save();
+}
